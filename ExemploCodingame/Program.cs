acdfef301bece3c64382b1c8b328ff79f8d53a8d
Thone <thonecardoso@gmail.com>
2021-10-14 09:51:13 -0300

    Exemplos

---
+++ ExemploCodingame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace ExemploCodingame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entrada = "-2 4 -654 345 108 -109";
+
+            var a = entrada.Split()
+                .Select(n => n[0] == '-' ? n.Substring(1) : n)
+                .Select(n => int.Parse(new string(n.Reverse().ToArray())))
+                .OrderByDescending(n => n).First();
+
+            Console.WriteLine(a);
+
+
+        }
+    }
+}
---
+++ ExemploQueryLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ExemploQueryLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] i = { 1, 2, 3, 4, 5, 6 };
+            var x = from a in i
+                where a % 2 == 0
+                select a;
+
+            Console.WriteLine(string.Join(" ", x));
+        }
+    }
+}
